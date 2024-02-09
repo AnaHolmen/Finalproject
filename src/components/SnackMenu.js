2b81466d44aafa9c8b4c3f935468b0849175e2c5
AnaHolmen <holmen.a14@gmail.com>
2024-02-08 21:06:19 -0600

    fixed snack page

--- src/components/SnackMenu.js
+++ src/components/SnackMenu.js
@@ -3,40 +3,42 @@ import React from "react";
 function SnackMenu() {
   // Define your snack data
   const snacks = [
-    { class: "Hotdog", price: "$5.00" },
-    { class: "Popcorn", price: "$4.00" },
-    { class: "Candy", price: "$3.00" },
-    { class: "Soda", price: "$2.50" },
+    { Snack: "Hotdog", price: "$5.00" },
+    { Snack: "Popcorn", price: "$4.00" },
+    { Snack: "Candy", price: "$3.00" },
+    { Snack: "Soda", price: "$2.50" },
   ];
 
+  const snackImageUrl =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRstdq8JU1cjiQhZ6JItEDikYk_Xj2sp0Dp5g&usqp=CAU";
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Class</th>
-          <th scope="col">Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {snacks.map((snack, index) => (
-          <tr
-            key={index}
-            className={index % 2 === 0 ? "table-primary" : "table-secondary"}
-          >
-            <th scope="row">{snack.class}</th>
-            <td>{snack.price}</td>
+    <div className="container" style={{ maxWidth: "1000x" }}>
+      <img
+        src={snackImageUrl}
+        alt="Snack"
+        style={{ maxWidth: "100%", maxHeight: "200px" }}
+      />
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Class</th>
+            <th scope="col">Price</th>
           </tr>
-        ))}
-        <tr className="table-primary">
-          <th scope="row">Primary</th>
-          <td>Cell</td>
-        </tr>
-        <tr className="table-secondary">
-          <th scope="row">Secondary</th>
-          <td>Cell</td>
-        </tr>
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {snacks.map((snack, index) => (
+            <tr
+              key={index}
+              className={index % 2 === 0 ? "table-primary" : "table-secondary"}
+            >
+              <th scope="row">{snack.class}</th>
+              <td>{snack.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/pages/Snacks.js
+++ src/pages/Snacks.js
@@ -3,7 +3,7 @@ import SnackMenu from "../components/SnackMenu";
 
 function SnackPage() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>Snack Page</h1>
       <SnackMenu />
     </div>
