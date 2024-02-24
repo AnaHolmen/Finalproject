5e9bf5966b74ca1021ccb975b42692c79331d31a
AnaHolmen <holmen.a14@gmail.com>
2024-02-23 19:35:30 -0600

    added some changes

--- src/pages/Posters.js
+++ src/pages/Posters.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import MovieLists from "../components/MovieLists";
-import ReviewForm from "../components/ReviewForm"; // Import the ReviewForm component
 import { week12API } from "../rest/week16Api";
 
 function Posters() {
@@ -18,22 +17,13 @@ function Posters() {
     fetchReviews();
   }, []);
 
-  const addReview = async (newReview) => {
-    try {
-      setReviews([...reviews, newReview]);
-    } catch (error) {
-      console.error("Error adding review:", error);
-    }
-  };
-
   return (
     <div>
       <h1>Posters</h1>
       <p>This shows the movie posters</p>
+
       {/* Pass reviews data to MovieLists component */}
       <MovieLists reviews={reviews} />
-      {/* Render the ReviewForm component */}
-      <ReviewForm addReview={addReview} /> {/* Pass the addReview function */}
     </div>
   );
 }
